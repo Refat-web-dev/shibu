4658530166ad6712d45737e05b2d0dc8bfdedb2c
main <refabatya@gmail.com>
2023-02-16 10:21:47 +0500

    main

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let name = 'Aleks'
+let age = '18'
+let gender = 'male'
+let gps = 'Samakarqand'
+console.log(
+    `I am ${ name} and i am ${age} my gender is ${gender} my geolocation is ${gps}`
+)
